6a2c74ea0a14e9a4e12e21165cee6a225f5317d0
TitoJosu23 <josuearmas00z@gmail.com>
2022-03-01 00:32:40 +0000

    adding login

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -19,7 +19,7 @@ const Layout = () => {
   const basename = process.env.BASENAME || "";
 
   return (
-    <div>
+    <div> 
       <BrowserRouter basename={basename}>
         <ScrollToTop>
           <Navbar />
